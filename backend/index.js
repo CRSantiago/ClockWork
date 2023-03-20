e6dbeb6584f1186d8d502fefc963ab3b21255eda
Chris Santiago <chrisantiago217@gmail.com>
2023-03-19 22:26:26 -0400

    Heroku setup

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,27 @@
-import app from "./server.js";
-import mongodb from "mongodb";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import UsersDAO from "./dao/usersDAO.js";
+import app from './server.js'
+// import mongodb from "mongodb";
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+// import UsersDAO from "./dao/usersDAO.js";
 
-dotenv.config();
+dotenv.config()
 //const MongoClient = mongodb.MongoClient;
 
-const port = process.env.PORT|| 8000; //8000 if env can't be accessed
-mongoose.connect(
-    process.env.RESTCLOCKWORK_DB_URI,
-    {
-        MaxPoolSize: 50,           // maximum num users connected at a time
-        wtimeoutMS: 2500,       // timeout request after amt of time
-        useNewUrlParser: true
+const port = process.env.PORT || 8000 //8000 if env can't be accessed
+mongoose
+  .connect(process.env.RESTCLOCKWORK_DB_URI, {
+    MaxPoolSize: 50, // maximum num users connected at a time
+    wtimeoutMS: 2500, // timeout request after amt of time
+    useNewUrlParser: true,
+  })
+  .catch((err) => {
+    console.error(err.stack)
+    process.exit(1)
+  })
+  .then(async (client) => {
+    //await UsersDAO.injectDB(client);
+    app.listen(port, () => {
+      // start webserver
+      console.log(`listening on port ${port}`)
     })
-    .catch(err=>{
-        console.error(err.stack);
-        process.exit(1);
-    })
-    .then(async client => {
-        //await UsersDAO.injectDB(client);
-        app.listen(port, () =>{      // start webserver
-            console.log(`listening on port ${port}`);
-        });
-    });
+  })
