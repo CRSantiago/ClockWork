dadaebfaa763a8eda8efa15f3325a39102a4d7d9
Chris Santiago <chrisantiago217@gmail.com>
2023-03-19 20:55:00 -0400

    Herokue setup

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,20 @@
-import express from "express";
-import cors from "cors";
-import clockwork from "./api/clockwork.route.js";
+import express from 'express'
+import cors from 'cors'
+import clockwork from './api/clockwork.route.js'
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
+const app = express()
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('frontend/build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  })
+}
+app.use(cors())
+app.use(express.json())
 
 // routing
-app.use("/api/v1/clockwork", clockwork);
-app.use("*", (req,res) => res.status(404).json({ error: "not found"}));
+app.use('/api/v1/clockwork', clockwork)
+app.use('*', (req, res) => res.status(404).json({ error: 'not found' }))
 
-export default app;
+export default app
