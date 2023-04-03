31acd9f2cb91650bbc1a5dcccb6ea9a7e6b587b4
CRSantiago <42612374+CRSantiago@users.noreply.github.com>
2023-04-03 15:01:32 -0400

    Attempt at heroku fix. Reference error: __dirname is not defined

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('frontend/build'))
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'))
   })
 }
 app.use(cors())
