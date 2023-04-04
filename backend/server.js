453b4549128570188ea23ace63f3baaa0b638d81
Chris Santiago <chrisantiago217@gmail.com>
2023-04-04 12:38:28 -0400

    Get Calendar initial commit

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,19 @@ import path from 'path'
 
 const app = express()
 if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('frontend/build'))
+  // // Set static folder
+  // app.use(express.static('frontend/build'))
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'))
+  // })
+  // Serve static files from the build directory
+  const buildPath = process.env.BUILD_PATH || 'frontend/build'
+  app.use(express.static(path.join(process.cwd(), buildPath)))
+
+  // Serve the index.html file for all other requests
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    const indexPath = path.join(process.cwd(), buildPath, 'index.html')
+    res.sendFile(indexPath)
   })
 }
 app.use(cors())
