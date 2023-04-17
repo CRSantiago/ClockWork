c5be1a6092f540eddd3f1e1fb817eb974bbb3688
CRSantiago <42612374+CRSantiago@users.noreply.github.com>
2023-04-17 15:35:21 -0400

    Dialogs for deletion

--- frontend/src/Components/Task.js
+++ frontend/src/Components/Task.js
@@ -3,10 +3,14 @@ import { useNavigate } from "react-router-dom"
 
 import { getTask } from "../data/getTask"
 import { deleteTask } from "../data/deleteTask"
+import DeleteConfirmationDialog from "./Dialogs/DeleteDialog"
 import "./Task.css"
 
 function Task({ task }) {
   const navigate = useNavigate()
+
+  const [showConfirmation, setShowConfirmation] = useState(false)
+
   function handleDetails() {
     console.log("details button clicked!")
     getTask(task.taskid).then((response) => {
@@ -14,29 +18,55 @@ function Task({ task }) {
       navigate("/TaskDetail", { state: { taskInfo: response.data } })
     })
   }
-  function handleDelete() {
+
+  function handleDeleteClick() {
+    setShowConfirmation(true)
+  }
+
+  function handleCancelClick() {
+    setShowConfirmation(false)
+  }
+
+  function handleConfirmDelete() {
     console.log("delete button clicked!")
-    const shouldDelete = window.confirm(
-      `Are you sure you want to delete the task: ${task.title}?`
-    )
-    if (shouldDelete) {
-      deleteTask({
-        id: localStorage.getItem("userid"),
-        taskId: task.taskid,
-      }).then((response) => console.log(response))
-    }
+    deleteTask({
+      id: localStorage.getItem("userid"),
+      taskId: task.taskid,
+    }).then((response) => {
+      if (response.status === 200) {
+        alert("task deleted!")
+        navigate("/calendar")
+      } else {
+        alert("task could not be deleted!")
+      }
+      console.log(response)
+    })
+
+    //}
   }
   return (
     <div className="taskContainer">
       <div className="taskDiv">
         <h3>{task.title}</h3>
         <div className="buttonDiv">
-          <button className="detailsButton" onClick={handleDetails}>
-            Details
-          </button>
-          <button className="deleteButton" onClick={handleDelete}>
-            Delete
-          </button>
+          {!showConfirmation && (
+            <div className="actionsDiv">
+              <button className="detailsButton" onClick={handleDetails}>
+                Details
+              </button>
+              <button className="deleteButton" onClick={handleDeleteClick}>
+                Delete
+              </button>
+            </div>
+          )}
+          {showConfirmation && (
+            <div className="overlay">
+              <DeleteConfirmationDialog
+                onConfirm={handleConfirmDelete}
+                onCancel={handleCancelClick}
+              />
+            </div>
+          )}
         </div>
       </div>
     </div>
