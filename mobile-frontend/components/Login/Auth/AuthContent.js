8877bd71483ebb69fd7e301017c5fa02850acdbf
Chris Santiago <chrisantiago217@gmail.com>
2023-05-01 09:56:55 -0400

    Added KeyboardAwareScrollView and further validation on register

--- mobile-frontend/components/Login/Auth/AuthContent.js
+++ mobile-frontend/components/Login/Auth/AuthContent.js
@@ -1,14 +1,7 @@
 import { useState } from 'react'
-import {
-  Alert,
-  StyleSheet,
-  View,
-  Image,
-  Text,
-  KeyboardAvoidingView,
-  ScrollView,
-} from 'react-native'
+import { Alert, StyleSheet, View, Image, Text } from 'react-native'
 import { navigator, useNavigation } from '@react-navigation/native'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 
 import FlatButton from '../ui/FlatButton'
 import AuthForm from './AuthForm'
@@ -33,6 +26,33 @@ function AuthContent({ isLogin, onAuthenticate, signUpSuccessMessage }) {
     }
   }
 
+  const passwordRegex =
+    /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[a-zA-Z\d!@#$%^&*]{8,}$/
+
+  function validatePassword(passwordTest) {
+    if (!passwordRegex.test(passwordTest)) {
+      const feedback = []
+      if (!/(?=.*[a-z])/.test(passwordTest)) {
+        feedback.push('at least one lowercase letter')
+      }
+      if (!/(?=.*[A-Z])/.test(passwordTest)) {
+        feedback.push('at least one uppercase letter')
+      }
+      if (!/(?=.*\d)/.test(passwordTest)) {
+        feedback.push('at least one number')
+      }
+      if (!/(?=.*[!@#$%^&*])/.test(passwordTest)) {
+        feedback.push('at least 1 special character')
+      }
+      if (passwordTest.length < 8) {
+        feedback.push('at least 8 characters')
+      }
+      Alert.alert('Invalid Password', feedback.join('\n'))
+      return false
+    }
+    return true
+  }
+
   function submitHandler(credentials) {
     let { isLogin, username, email, confirmEmail, password, confirmPassword } =
       credentials
@@ -45,7 +65,7 @@ function AuthContent({ isLogin, onAuthenticate, signUpSuccessMessage }) {
       password = password.trim()
 
       const emailIsValid = email.includes('@')
-      const passwordIsValid = password.length > 6
+      const passwordIsValid = validatePassword(password)
       const emailsAreEqual = email === confirmEmail
       const passwordsAreEqual = password === confirmPassword
 
@@ -54,7 +74,20 @@ function AuthContent({ isLogin, onAuthenticate, signUpSuccessMessage }) {
         !passwordIsValid ||
         (!isLogin && (!emailsAreEqual || !passwordsAreEqual))
       ) {
-        Alert.alert('Invalid input', 'Please check your entered credentials.')
+        let returnError = ''
+        if (!emailIsValid) {
+          returnError +=
+            'You entered an invalid email. Please try again.' + '\n'
+        } else if (!emailsAreEqual) {
+          returnError +=
+            'The emails you entered do not match. Please try again.' + '\n'
+        } else if (!passwordsAreEqual) {
+          returnError +=
+            'The passwords you entered do not match. Please try again.' + '\n'
+        }
+        if (returnError.length > 0) {
+          Alert.alert('Invalid input', returnError)
+        }
         setCredentialsInvalid({
           email: !emailIsValid,
           confirmEmail: !emailIsValid || !emailsAreEqual,
@@ -69,7 +102,7 @@ function AuthContent({ isLogin, onAuthenticate, signUpSuccessMessage }) {
 
   return (
     <View style={styles.authContent}>
-      <ScrollView>
+      <KeyboardAwareScrollView>
         <Text style={styles.heading}>Welcome to Clockwork</Text>
         <Image
           style={styles.image}
@@ -86,7 +119,7 @@ function AuthContent({ isLogin, onAuthenticate, signUpSuccessMessage }) {
             {isLogin ? 'Create a new user' : 'Log in instead'}
           </FlatButton>
         </View>
-      </ScrollView>
+      </KeyboardAwareScrollView>
     </View>
   )
 }
